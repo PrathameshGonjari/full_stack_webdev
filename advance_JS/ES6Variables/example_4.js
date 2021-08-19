2c4f0047e9ea961748de3936a84cb3d0a62fbc02
Prathamesh <prathameshgonjari@gmail.com>
2021-08-20 00:30:02 +0530

    ES6 variables

---
+++ advance_JS/ES6Variables/example_4.js
@@ -0,0 +1,7 @@
+var firstname = 'Prathamesh';
+//var can be re declared and assingned
+let  lastname = 'Gonjari';
+//let can be re assingned but cannot be redeclared
+const salary = 5000;
+//constant cannot be re-declared and assingned 
+console.log('My Name is '+firstname+ ' ' +lastname+ ' and salary is ' +salary)
