4b4e7bf9c80777cff02a94b89ea941972b673632
Prathamesh <prathameshgonjari@gmail.com>
2021-08-20 00:42:02 +0530

    classes

---
+++ advance_JS/classes/example_1.js
@@ -0,0 +1,20 @@
+class Worker{
+
+  getsalary(){
+    console.log("Employee Salary is 15000");
+  }
+
+  getattendance(){
+    console.log("Attendacne of Employee is 15 days");
+  }
+
+  getholiday(){
+    console.log("Holiday taken by Employee is 5 days");
+  }
+
+}
+
+e1 = new Worker();
+e1.getsalary();
+e1.getattendance();
+e1.getholiday();
